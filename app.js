b5bfcd2c73b0ab533e7c2f2699c9e28dd32a12f1
alpardfm <tokferdian100@gmail.com>
2021-10-08 02:18:44 +0700

    Routes Documentation

--- app.js
+++ app.js
@@ -9,23 +9,30 @@ const swaggerUi = require('swagger-ui-express')
 require('dotenv/config')
 require('./config/db.config')
 
-//Dokumentasi
-const swg = require('./config/doc.config')
-const swaggerDocs = swaggerJsDocs(swg)
 
 //Middleware
 app.use(bodyParser())
 app.use(cors())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
+
+//import Dokumentasi
+const swgProduk = require('./app/documentation/produk.doc')
+const swgPembelian = require('./app/documentation/pembelian.doc')
+const swgDistributor = require('./app/documentation/distributor.doc')
+
+const swaggerProduk = swaggerJsDocs(swgProduk)
+const swaggerPembelian = swaggerJsDocs(swgPembelian)
+const swaggerDistributor = swaggerJsDocs(swgDistributor)
 
 //import routes
 const produk = require('./app/controller/produk.controller')
 const distributor = require('./app/controller/distributor.controller')
 const pembelian = require('./app/controller/pembelian.controller')
 
-
-
+//routes-dokumentasi
+app.use('/api-docs/produk', swaggerUi.serve, swaggerUi.setup(swaggerProduk))
+app.use('/api-docs/pembelian', swaggerUi.serve, swaggerUi.setup(swaggerPembelian))
+app.use('/api-docs/distributor', swaggerUi.serve, swaggerUi.setup(swaggerDistributor))
 
 //routes
 app.use('/produk', produk)
